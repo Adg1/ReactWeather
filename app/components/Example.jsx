3a02b9d6daa1f40e083beccf98992f292214515b
Adg1 <amandeepgupta1@gmail.com>
2017-03-19 04:23:21 +0530

    Rename examples page

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -2,7 +2,10 @@ const React = require('react');
 
 var Example = (props) => {
   return (
-    <h3>various examples</h3>
+    <div>
+      <h3>Examples</h3>
+      <p>Welcome to exmaples page!</p>
+    </div>
   );
 };
 
